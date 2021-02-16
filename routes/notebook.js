bb8ef8917005c82030af474165eebdb013b9e21d
hakankaan <kaanhakan2005@gmail.com>
2021-02-16 20:59:45 +0300

    3 yeni endpoint eklendi
    Notebook listesi alma, istenilen notebooka ait içeriği alma ve notebook silmek için gerekli endpointler oluşturuldu.

--- routes/notebook.js
+++ routes/notebook.js
@@ -2,6 +2,36 @@ var express = require("express");
 var router = express.Router();
 const fs = require("fs");
 
+router.get("/", function (req, res) {
+    try {
+      fs.readdir("notebooks/", (err, files) => {
+        if (err) res.status(400).send(err);
+        res.status(200).send(files);
+      });
+    } catch (err) {
+      res.status(500).end();
+    }
+  });
+  
+  router.post("/getcontent", function (req, res) {
+    try {
+      const name = req.body.name;
+      if (!fs.existsSync(`notebooks/${name}.txt`)) {
+        res.status(204).end();
+      } else {
+        fs.readFile(`notebooks/${name}.txt`, 'utf8', function(err, data) {
+          if (err) res.status(204).send(err);
+          res.status(200).send({
+            name: name,
+            content: data
+          });
+        });
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
 
 router.post("/create", function (req, res) {
 
@@ -36,5 +66,18 @@ router.post("/create", function (req, res) {
       res.status(500).send(err);
     }
   });
+
+  router.delete("/delete", function (req, res) {
+    try {
+      const name = req.body.name;
+      fs.unlink(`notebooks/${name}.txt`, (err) => {
+        if (err) res.status(400).send(err);
+        res.sendStatus(200);
+      });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+  
 
 module.exports = router;
